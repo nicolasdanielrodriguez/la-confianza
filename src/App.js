1fa3a11d8b95169a161438ca4ccc190d23839b17
nicolasdanielrodriguez <nicolasdanielrodriguez@gmail.com>
2021-11-22 00:41:19 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          La Confianza<code> Fábrica de pastas</code>
+          La Confianza2<code> Fábrica de pastas</code>
+          
         </p>
         <a
           className="App-link"
