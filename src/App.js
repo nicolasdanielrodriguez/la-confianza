e06504af6a1e805b5a0b9b122793dfe751d3d3bd
nicolasdanielrodriguez <nicolasdanielrodriguez@gmail.com>
2022-04-21 20:05:42 -0300

    Clase 4 Componentes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar.js'
 import './components/NavBar/NavBar.css'
-import './components/ListItem/ListItemContainer'
+import ListItemContainer from './components/ListItemContainer/ListItemContainer';
+import Body from './components/botonComprar';
+
+
 function App() {
   return (
     <div className="App">
+     
       <NavBar/>
-      
+      <ListItemContainer texto='producto 1'/>
+      <ListItemContainer texto='producto 2'/>
+      <ListItemContainer/>
+      <Body color='green'>Hola 2</Body>
+      <Body color='red'>Hola 1</Body>
     </div>
   );
 }
---
+++ src/components/botonComprar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './botonComprar.css'
+
+export default function Body({color}) {
+  return (
+      <>
+    <input type="text"/>
+    <button style= {{color:color}}>Hola</button>
+    </>
+  )
+}
